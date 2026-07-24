ddff0b1727dbd2e2e6f293d92c9592b30b43ebe1
TheNikhilSharma <nikhilsharmalku@gmail.com>
2026-07-24 23:16:56 +0530

    imp

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -13,7 +13,7 @@ async function dbConnect(): Promise<void> {
     }
 
     try {
-        // const db = await mongoose.connect("mongodb+srv://nikhilsharmalku:K1Kk8dGIRJcGJjKd@aims.tnosh.mongodb.net/aims")
+        // const db = await mongoose.connect("mongodb+srv://username:pass@aims.tnosh.mongodb.net/aims")
         const db = await mongoose.connect(process.env.MONGO_URI as string);
 
         connection.isConnected = db.connections[0].readyState;
